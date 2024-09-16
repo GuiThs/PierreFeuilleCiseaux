7828769982b256886804223d94f2861b1bdfb7b3
GuiThs <g.thomas83200@gmail.com>
2024-09-16 15:37:12 +0200

    Mise a jour de gestion

--- public/src/module/gestion.py
+++ public/src/module/gestion.py
@@ -7,9 +7,9 @@ def pierre_feuille_ciseaux():
     options = ['pierre', 'feuille', 'ciseaux']
     
     while True:
-        user_choice = input("Choisissez Pierre, Feuille ou Ciseaux : ").lower()
+        user_choice = input("Choisis Pierre, Feuille ou Ciseaux : ").lower()
         if user_choice not in options:
-            print("Choix invalide. Essayez encore.")
+            print("Choix invalide. Essaye encore.")
             continue
         
         # Choix aléatoire de l'ordinateur avec numpy
@@ -22,16 +22,16 @@ def pierre_feuille_ciseaux():
         elif (user_choice == "pierre" and computer_choice == "ciseaux") or \
              (user_choice == "feuille" and computer_choice == "pierre") or \
              (user_choice == "ciseaux" and computer_choice == "feuille"):
-            print("Vous gagnez !")
+            print("Tu gagnes !")
             """L'antislash est une continuation de ligne mais,
             il y avait possibilié de faire un tuple pour imbriquer ces 3 users choices
             """
             
         else:
-            print("Vous perdez !")
+            print("Tu perds !")
         
         
-        replay = input("Voulez-vous rejouer ? O/N : ").lower() # "o" si le joueur veut rejouer
+        replay = input("On rejoue ? O/N : ").lower() # "o" si le joueur veut rejouer
         if replay != "o":
             print("Merci d'avoir joué !")
             break
